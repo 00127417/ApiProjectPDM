29ac4f99852634f90aaf556f2bf4f5236a07bbca
00127417 <00127417@uca.edu.sv>
2019-06-17 10:21:37 -0600

    se agregó progress

--- models/Patient.js
+++ models/Patient.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+    Schema = mongoose.Schema,
+    progress = require('../models/Progress');
 
 let Patient = new Schema({
     name: {type: String, required: true},
     date: {type: String, required: true},
-    level: {type: Number, required: true}
+    level: {type: Number, required: true},
+    progress: progress.schema
 });
 
 module.exports = mongoose.model('patient', Patient)
