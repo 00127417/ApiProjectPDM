ca6ab363c957a4214e8ad05b6a1c2c734c13ff7d
00127417 <00127417@uca.edu.sv>
2019-06-19 13:32:44 -0600

    se agrego un atributo a model user y se le agregan los item al paciente

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose'),
 
 let User = new Schema({
     username: {type: String, required: true, unique: true},
+    email: {type: String, required: true},
     password: {type: String, required: true},
     type: {type: Number, required: true},
     patients: [patient.schema]
